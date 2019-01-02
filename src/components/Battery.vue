c06d4c07678267ef4f47575b238bf1f5db70340c
Bernhard Richter <bernhard.richter@gmail.com>
2019-01-02 13:31:34 +0100

    Battery level component

---
+++ src/components/Battery.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="battery">
+    <div class="battery-level"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+
+@Component
+export default class Battery extends Vue{
+
+    mounted(){
+
+
+    }
+}
+</script>
+
+<style scoped>
+.battery {
+  background-color: #fff;
+  border: 2px solid #000;
+  height: 32px;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  width: 100px;
+}
+
+.battery:after {
+  background-color: #fff;
+  border: 2px solid #000;
+  content: "";
+  height: 16px;
+  position: absolute;
+  right: -12px;
+  top: 6px;
+  width: 4px;
+}
+
+.battery .battery-level {
+  background-color: #666;
+  height: 100%;
+  width: 10%;
+}
+</style>
--- src/components/Zones.vue
+++ src/components/Zones.vue
@@ -1,17 +1,28 @@
 <template>
+    <div>Location {{ $route.params.location }}
+        <Battery></Battery>
+    </div>
 
-    <div>Location {{ $route.params.location }}</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import { getZones, Zone } from "../web-api";
+import Battery from '@/components/Battery.vue';
 
-@Component
+@Component({
+    components: {
+        Battery
+    }
+}
+)
 export default class Zones extends Vue{
+    zones : Zone[] = new Array<Zone>();
+
     async mounted(){
         let locationParam = this.$route.params['location'];
+        this.zones = await getZones(locationParam);
     }
 }
 </script>
